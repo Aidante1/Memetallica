ddad5f6f256aa0eb10f2e914ee3e5b9329a21dd3
Hasty-Toaster <Alexander@INSPIRON_518>
2021-01-30 11:54:53 -0500

    changed some textures, added metals

--- src/main/java/net/mcreator/memetallica/item/BeanieItem.java
+++ src/main/java/net/mcreator/memetallica/item/BeanieItem.java
@@ -1,21 +1,33 @@
 
 package net.mcreator.memetallica.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.memetallica.MemetallicaModElements;
+
 @MemetallicaModElements.ModElement.Tag
 public class BeanieItem extends MemetallicaModElements.ModElement {
-
 	@ObjectHolder("memetallica:beanie_helmet")
 	public static final Item helmet = null;
-
 	@ObjectHolder("memetallica:beanie_chestplate")
 	public static final Item body = null;
-
 	@ObjectHolder("memetallica:beanie_leggings")
 	public static final Item legs = null;
-
 	@ObjectHolder("memetallica:beanie_boots")
 	public static final Item boots = null;
-
 	public BeanieItem(MemetallicaModElements instance) {
 		super(instance, 16);
 	}
@@ -52,16 +64,11 @@ public class BeanieItem extends MemetallicaModElements.ModElement {
 				return 0f;
 			}
 		};
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "memetallica:textures/models/armor/beanie__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("beanie_helmet"));
-
 	}
-
 }
